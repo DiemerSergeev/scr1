1d96d602163a0b73432de3bd9764dd92325445a9
Dmitrii Sergeev <dimon2002st.peterburg@gmail.com>
2025-04-23 22:44:44 +0300

    Add files via upload

--- sim/tests/common/riscv_macros.h
+++ sim/tests/common/riscv_macros.h
@@ -102,24 +102,36 @@
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
 #define RVTEST_CODE_BEGIN                                               \
+        .org 0x0,0;                                                   \
+        MSG_TRAP:                                                       \
+        .string "Break point";                        \ 
         .section .text.init;                                            \
-        .org 0xC0, 0x00;                                                \
         .balign  64;                                                    \
         .weak stvec_handler;                                            \
         .weak mtvec_handler;                                            \
 trap_vector:                                                            \
         /* test whether the test came from pass/fail */                 \
         csrr a4, mcause;                                                \
-        li a5, CAUSE_USER_ECALL;                                        \
+        /*li a5, CAUSE_USER_ECALL;                                        \
         beq a4, a5, _report;                                            \
         li a5, CAUSE_SUPERVISOR_ECALL;                                  \
-        beq a4, a5, _report;                                            \
-        li a5, CAUSE_MACHINE_ECALL;                                     \
-        beq a4, a5, _report;                                            \
+        beq a4, a5, _report;   */                                         \
+        li a5, CAUSE_BREAKPOINT;                                     \
+        /*beq a4, a5, _report; */                                         \
+        /* init for loop, 0xf0000000 address for print */               \
+        lui a6, 0xf0000;                                                \
+        la a7, MSG_TRAP;                                                \
+next_iter:                                                              \
+        lb a5, 0(a7);                                                   \
+        beq a5, x0, break_from_loop;                                    \
+        sw a5, 0(a6);   /* write to a6 char for print */                \
+        addi a7, a7, 1;                                                 \
+        jal x0,next_iter;                                               \
+break_from_loop:                                                        \
         /* if an mtvec_handler is defined, jump to it */                \
         la a4, mtvec_handler;                                           \
         beqz a4, 1f;                                                    \
-        jr a4;                                                          \
+        j _report;                                                          \
         /* was it an interrupt or an exception? */                      \
 1:      csrr a4, mcause;                                                \
         bgez a4, handle_exception;                                      \
@@ -133,6 +145,7 @@ _report:                                                                \
         j sc_exit;                                                      \
         .balign  64;                                                    \
         .globl _start;                                                  \
+        .section .text.start;                                           \
 _start:                                                                 \
         RISCV_MULTICORE_DISABLE;                                        \
         /*INIT_SPTBR;*/                                                 \
@@ -151,7 +164,8 @@ _start:                                                                 \
                (1 << CAUSE_FETCH_PAGE_FAULT) |                          \
                (1 << CAUSE_MISALIGNED_FETCH) |                          \
                (1 << CAUSE_USER_ECALL) |                                \
-               (1 << CAUSE_BREAKPOINT);                                 \
+               (1 << CAUSE_BREAKPOINT) |                                \
+               (1 << CAUSE_MACHINE_ECALL);                                 \
         csrw medeleg, t0;                                               \
         csrr t1, medeleg;                                               \
         bne t0, t1, other_exception;                                    \
@@ -816,4 +830,3 @@ pass: \
 #define TEST_DATA
 
 #endif
-
